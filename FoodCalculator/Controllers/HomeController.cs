1187e6b113d2f7dbfa5da5d13ad131f4f95c4f73
mulanecka <mulanecka@gmail.com>
2017-01-20 12:29:09 +0100

    Initial commit

---
+++ FoodCalculator/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using FoodCalculator.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product product)
+        {
+            return View("ProductAdded");
+        }
+    }
+}
---
+++ FoodCalculator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodCalculator.MvcApplication" Language="C#" %>
